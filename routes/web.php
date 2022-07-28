194d488d116f8e7e6a0ea23571c36dffee12b3ed
junaid-7860565244 <110153624+junaid-7860565244@users.noreply.github.com>
2022-07-28 18:25:10 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,29 @@ Route::controller(EmpController::class)->group(function () {
    Route::post('/employee/update', 'update')->name('update');
      Route::post('/employee/deleteuser/{id}',  'deleteuser')->name('deleteuser');
 
+    });
+
+
+      Route::group(['namespace' => 'App\Http\Controllers'], function()
+    {   
+
+     Route::get('/', 'EmpController')->name('home.index');
+     Route::group(['middleware' => ['guest']], function() {
+          
+     Route::get('/register', 'EmpController')->name('register.show');
+     Route::post('/register', 'EmpController')->name('register.perform');
+
+       
+        Route::get('/login', 'EmpController')->name('login.show');
+        Route::post('/login', 'EmpController')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+    Route::get('/logout', 'EmpController')->name('logout.perform');
+    });
+});
+
     
 
 
